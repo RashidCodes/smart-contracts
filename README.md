7993934fc1092742b01d603d3ae9664836aa3270
Rashid <rashid@Rashids-MacBook-Pro-2.local>
2022-05-31 23:25:11 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Solidity, Blockchain, and Smart Contracts Course: My Perspective
+
+<br/>
+
+# Metamask
+
+Metamask is a non-custodial cryptocurrency and token wallet. Google it to learn how to install it as a browser extension in your favourite browser.
+
+# Etherscan
+
+You can use a tool like ```Etherscan``` to view different addresses and what's been going on with them. It's also know as a **block explorer**.
+
+ 
+## Gas 
+Gas refers to the fee paid to node operators for successfully include a node transaction in a blockchain. Whenever we do something on the blockchain, it costs gas.
+
+
+# Rinkeby Faucet
+Rinkeby is going to be one of two testnets that we're going to be workign with. The other testnet is koven. It's important to know how to switch between testnets and other EVM compatible chains. Go [here](https://rinkebyfaucet.com/) to get 0.1ETH per day for free.
+
+
+# Eth Gas Station 
+It is a gas estimator of the blockchain. You can find it [here](ethgasstation.info)
+
+
+# Remix
+Remix IDE is where we'll learn to use solidity and smart contracts.
---
+++ first_smart_contract.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+// you can also specify other versions like this
+// pragma solidity >=0.6.0 <0.9.0 (versions greater than or equal to 0.6.0 and less than 0.9.0
+
+
+// create a contract: Similar to a class in JS
+contract SimpleStorage {
+
+    // let's go into some of the types of solidity
+    // bool favouriteBool = false;
+    // string favouriteString = "String";
+    // int256 favouriteInt = -5; // can be positive/negative
+    // address favouriteAddress = 0x274f5eE14cCc95a092C05a84e4fdD29a8E33D9Dd;
+    // bytes32 favouriteBytes = "cat";
+
+
+    // Automatically initialised to 0
+    uint256 favouriteNumber; // this technically is a view function
+
+    // Structs 
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    
+    // create a list of People 
+    People[] public people;
+
+    // a mapping (hash map)
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    // you can also create arrays of a fixed size 
+    // People[1] public people;
+
+
+    // assign a value to favouriteNumber
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    // non-state changing keywords: view, pure
+    function retrieve() public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    /* Memory: The data will only be stored during the execution of the function or contract call
+       Storage: The data will persist even after the function executes
+
+       If you use a string parameter for one of your functions, you need to 
+       give it a type of string memory
+    */
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(People(_favouriteNumber, _name));
+
+        // add to the mapping 
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+
+    // pure functions are functions that basically do some type of math 
+    // function do_some_math(uint256 favouriteNumber) public pure {
+    //     favouriteNumber + 2;
+    // }
+
+}
+
+
+
